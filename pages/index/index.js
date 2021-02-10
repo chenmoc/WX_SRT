0d706ace0ead9fb09e01714df925bbc2f880472e
chenmoc <2199351134@qq.com>
2021-02-10 17:37:11 +0800

    1.0.1

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Be water my friend.',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
