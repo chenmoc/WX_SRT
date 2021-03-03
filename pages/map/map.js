ec79f4966670bda34a2a954ac947b70bc250bea6
chenmoc <2199351134@qq.com>
2021-03-03 16:25:47 +0800

    1.1.1

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,14 +1,32 @@
 // pages/map/map.js
 var lat1= 'null';
 var lng1 = 'null';
+
+function distance(lat1,long1,lat2,long2) {
+  var lat3 = lat1*Math.PI/180.0;
+  var lat4 = lat2*Math.PI/180.0;
+  var a = Math.abs(lat1-lat2) * Math.PI/180.0;
+  var b = Math.abs(long1-long2) * Math.PI/180.0;
+  var d = 2*Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2)+Math.cos(lat3)*Math.cos(lat4)*Math.pow(Math.sin(b/2),2)));
+  console.log(typeof(d));
+  d = d * 6370.856;
+  d = Math.round(d*10000)/10000;
+  return d;
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+      lat_4: 36.617216,
+      long_4: 116.952421,
+      lat_12: 36.646048,
+      long_12: 116.967579,
       long1: "n",
       lan1: "n",
+      dt: 0,
   },
 
   /**
@@ -34,8 +52,18 @@ Page({
       me.setData({
         'long1': lng1,
         'lan1': lat1
-      })
+      });
+      console.log(typeof(lat1));
+      console.log(typeof(me.data.lat_12));
     },5000
+      );
+      setTimeout(
+        function(){
+          me.setData({
+            'dt': Number(distance(lat1,lng1,me.data.lat_12,me.data.long_12))
+          });
+          console.log(distance(me.data.lat_4,me.data.long_4,me.data.lat_12,me.data.long_12)*1000);
+        },6000
       )
     //  document.getElementById("chen").innerHTML = "当前位置\n经度："+lat1+"\n维度："+lng1;
   },
@@ -93,3 +121,4 @@ Page({
 
   }
 })
+
